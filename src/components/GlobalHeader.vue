0740935e68d2597438e8b98822cc5804975856b8
HF <3123961845@qq.com>
2025-07-08 00:24:21 +0800

    图片列表功能

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -69,6 +69,11 @@ const originItems = [
     title: '用户管理',
   },
   {
+    key: '/admin/pictureManage',
+    label: '图片管理',
+    title: '图片管理',
+  },
+  {
     key: 'others',
     label: h('a', { href: 'https://www.codefather.cn', target: '_blank' }, '编程导航'),
     title: '编程导航',
---
+++ src/pages/PictureDtailPage.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div id = "pictureDetailPage">
+
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
